63fd608abf97c8c077dcb4c0bc1e101a4e712e76
cpd <120633269@qq.com>
2020-09-18 14:36:17 +0800

    add 109

---
+++ 109. 有序链表转换二叉搜索树.cpp
@@ -0,0 +1,137 @@
+#include <stack>
+#include <string>
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+#include <climits>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <time.h>
+#include <map>
+using namespace std;
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
+public:
+    TreeNode* dfs(vector<int>& temp, int l, int r) {
+        if (l > r)return nullptr;
+        int mid = (l + r) / 2;
+        TreeNode* root = new TreeNode(temp[mid]);
+        if (l == r)return root;
+        root->left = dfs(temp, l, mid - 1);
+        root->right = dfs(temp, mid+1, r);
+        return root;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (head == nullptr)return nullptr;
+        vector<int> temp;
+        ListNode* p = head;
+        while (p != nullptr)
+        {
+            temp.push_back(p->val);
+            p = p->next;
+        }
+        return dfs(temp, 0, temp.size() - 1);
+    }
+};
+void trimLeftTrailingSpaces(string& input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+        }));
+}
+
+void trimRightTrailingSpaces(string& input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+        }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for (int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string treeNodeToString(TreeNode* root) {
+    if (root == nullptr) {
+        return "[]";
+    }
+
+    string output = "";
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        if (node == nullptr) {
+            output += "null, ";
+            continue;
+        }
+
+        output += to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* head = stringToListNode(line);
+
+        TreeNode* ret = Solution().sortedListToBST(head);
+
+        string out = treeNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
