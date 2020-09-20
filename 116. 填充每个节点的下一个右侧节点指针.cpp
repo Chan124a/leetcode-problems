2970a007d1d7167576ece6046686029aa54f38a7
cpd <120633269@qq.com>
2020-09-20 14:02:45 +0800

    add 116

---
+++ 116. 填充每个节点的下一个右侧节点指针.cpp
@@ -0,0 +1,49 @@
+#include <stack>
+#include <string>
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+#include <climits>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <time.h>
+#include <map>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == NULL)return NULL;
+        auto left = root->left;
+        auto right = root->right;
+        while (left!=NULL)
+        {
+            left->next = right;
+            left = left->right;
+            right = right->left;
+        }
+        connect(root->left);
+        connect(root->right);
+        return root;
+    }
+};
