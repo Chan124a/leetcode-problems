d7dbab5f706baa29a43cd6a59d93a83c0403c7ea
cpd <120633269@qq.com>
2020-10-10 15:49:08 +0800

    add 145 144

---
+++ 145. 二叉树的后序遍历.cpp
@@ -0,0 +1,42 @@
+#include <stack>
+#include <string>
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+#include <climits>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <time.h>
+#include <map>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> ans;
+    void dfs(TreeNode* root) {
+        if (root == NULL)return;
+        dfs(root->left);
+        dfs(root->right);
+        ans.push_back(root->val);
+        return;
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
