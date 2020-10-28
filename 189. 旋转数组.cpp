5defbe9b817c2b56144eade83d459c6959006c91
cpd <120633269@qq.com>
2020-10-28 14:49:25 +0800

    2020-10-28

---
+++ 189. 旋转数组.cpp
@@ -0,0 +1,88 @@
+#include <stack>
+#include <string>
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+#include <climits>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <time.h>
+#include <map>
+#include <math.h>
+#include <bitset>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        std::reverse(nums.begin(), nums.begin() + k%nums.size());
+        std::reverse(nums.begin() + k % nums.size(), nums.end());
+        std::reverse(nums.begin(), nums.end());
+    }
+};
+void trimLeftTrailingSpaces(string& input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+        }));
+}
+
+void trimRightTrailingSpaces(string& input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+        }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for (int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int k = stringToInteger(line);
+
+        Solution().rotate(nums, k);
+
+        string out = integerVectorToString(nums);
+        cout << out << endl;
+    }
+    return 0;
+}
